4e8e48609c089205ffec1b1f5d4bd8311e176155
ItakatzI <katzir66@gmail.com>
2018-04-05 09:59:53 +0300

    deleted insurt script validator, it fucked up the load of the scrips, now im working on object instanses validation

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,13 +1,14 @@
 // ON APP ICON CLICK
 //if game has already been started dont load scripts again
 
+
 chrome.browserAction.onClicked.addListener(function (tab) {
-	if(typeof gameInPrograss === "undefined"){
 		// for the current tab, inject the "inject.js" file & execute it - accessins page DOM
-		chrome.tabs.executeScript(tab.id, { file: "src/class/class_hello_waldo.js" });
-		chrome.tabs.executeScript(tab.id, { file: "src/class/class_elements_manipulator.js" });
-		chrome.tabs.executeScript(tab.id, { file: "src/class/class_game.js" });
-		chrome.tabs.executeScript(tab.id, { file: "src/inject/inject.js" });
-		gameInPrograss = 1;
-	}
+chrome.tabs.executeScript(tab.id, { file: "src/class/class_hello_waldo.js" });
+chrome.tabs.executeScript(tab.id, { file: "src/class/class_elements_manipulator.js" });
+chrome.tabs.executeScript(tab.id, { file: "src/class/class_game.js" });
+chrome.tabs.executeScript(tab.id, { file: "src/inject/inject.js" });
+
+console.log("test");
+
 });
