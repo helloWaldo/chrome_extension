9eed92caef5a5162705ce9542b6daeea268ea32b
Dodge <dorefrati@gmail.com>
2018-04-24 19:58:57 +0300

    open a new branch for 3rdSprint and that is his name yo

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -8,6 +8,6 @@ chrome.tabs.executeScript(tab.id, { file: "src/class/class_injector.js"});
 chrome.tabs.executeScript(tab.id, { file: "src/class/class_elements_manipulator.js" });
 chrome.tabs.executeScript(tab.id, { file: "src/class/class_game.js" });
 chrome.tabs.executeScript(tab.id, { file: "src/class/class_gameEvents.js"});
-// Needs to be the last - src/inject/inject.js
+// Needs to be the last - src/inject/inject.js 
 chrome.tabs.executeScript(tab.id, { file: "src/inject/inject.js" });
 });
